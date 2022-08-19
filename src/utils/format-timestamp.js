9848e4a63d1e5ead1400b018079ee6bca07ed869
Evgeny Smagin <snailman7297@gmail.com>
2022-08-20 00:15:46 +0500

    gh page

--- src/utils/format-timestamp.js
+++
@@ -1,6 +0,0 @@
-const formatTimestamp = timestamp => {
-  const date = new Date(timestamp);
-  return `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`;
-};
-
-export default formatTimestamp;
